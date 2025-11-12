be970d814dd6060f530aa10788fb6737b6f02877
joshcr25 <Josh Christmas Rommlynn>
2025-11-12 21:30:43 +0700

    Menambah sedikit kode untuk mengintegrasikan Django dan Flutter

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import logout as auth_logout
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 from django.contrib.auth.models import User
 import json
 from django.shortcuts import render
@@ -69,3 +72,19 @@ def register(request):
             "status": False,
             "message": "Invalid request method."
         }, status=400)
+        
+@csrf_exempt
+def logout(request):
+    username = request.user.username
+    try:
+        auth_logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logged out successfully!"
+        }, status=200)
+    except:
+        return JsonResponse({
+            "status": False,
+            "message": "Logout failed."
+        }, status=401)
